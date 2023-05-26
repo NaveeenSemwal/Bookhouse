ed86d631a71e67ff0e31e116b3fc883ea5dda70e
NaveeenSemwal <semwal.dotnet@gmail.com>
2023-05-26 10:08:10 +0530

    Added proects

---
+++ src/Book.Schedular/Program.cs
@@ -0,0 +1,10 @@
+using Book.Schedular;
+// check youtube vedios of CodBeta
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
